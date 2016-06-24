0f2c50622f9d9764f47f4a05655053b9622d685d
joao <joaovitor199917@hotmail.com>
2016-06-23 21:23:12 -0300

    primeiro envio

---
+++ caixa eletronico/src/pg/caixa.java
@@ -0,0 +1,34 @@
+package pg;
+
+public class caixa {
+
+	int saque - 784;
+	int cem - o, cinquenta - 0, dez -, cinco - 0, dois - 0;
+	int temp;
+
+
+	temp = saque;
+	
+if (temp >= 100)
+cem = saque / 100;
+temp = saque % 100;
+
+if (temp >= 50)
+cinquenta = saque / 50;
+temp = temp % 50;
+
+if (temp >= 20)
+vinte = saque / 20;
+temp = temp % 20;
+
+if (temp >=10)
+dez = saque / 10;
+temp = temp % 10;
+
+if (temp >=2)
+dois = saque / 2;
+temp = temo % 2;
+
+
+}
+
